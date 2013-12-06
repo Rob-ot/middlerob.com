db31a9821b9bdd222f7a68df9e3b5860689264dd
Your Name <rob@i.tv>
2013-12-05 19:12:40 -0800

    update submodules on install

--- ggg.js
+++ ggg.js
@@ -1,6 +1,6 @@
 module.exports = {
   start: "./node_modules/.bin/coffee app.coffee",
-  install: "npm install",
+  install: "npm install && git submodule init && git submodule update",
 
   servers: {
     middlerob: "root@middlerob.com"
